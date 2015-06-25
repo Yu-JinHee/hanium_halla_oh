d82964d460465dc7f730a0c672d4aca1a8fba51e
Yu-JinHee <rjhrjh119@naver.com>
2015-06-26 01:09:36 +0900

    포맷 후

--- src/com/example/halla/Hangle.java
+++ src/com/example/halla/Hangle.java
@@ -1,13 +1,21 @@
 package com.example.halla;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Hangle extends Activity {
-
+	int index = (int)(Math.random()*6);
+	
+	int res = array[index];
+	 public static final int[] array= {
+	  R.drawable.apple, R.drawable.pencil, R.drawable.turtle,
+	  R.drawable.whale, R.drawable.ic_launcher,R.drawable.strawberry};
+	 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,31 @@ public class Hangle extends Activity {
         		finish();
         	}
         });
+        
+        /**이미지 삽입*/
+        // drawable 리소스에서 naverlogo 파일 호출 하여 Drawable에 등록
+        Drawable drawable = getResources().getDrawable(R.drawable.pencil);
+        // id : imageView01 <ImageView>를 가져온다.
+        // imageView01 에 리소스에서 가져온 naverlogo 등록
+        ImageView imageView = (ImageView)findViewById(R.id.imageView1);
+        imageView.setImageDrawable(drawable);
+      
+        /**정답확인 버튼*/
+        Button btn1 = (Button)findViewById(R.id.button1);
+        btn1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				//다음그림 뿌려주기
+				int index = (int)(Math.random()*6);
+				int res = array[index];
+				ImageView imageView = (ImageView)findViewById(R.id.imageView1);
+		        imageView.setImageResource(res); 
+			}
+		});
+      
 	}
 
 }
+
--- src/com/example/halla/MainActivity.java
+++ src/com/example/halla/MainActivity.java
@@ -2,13 +2,10 @@ package com.example.halla;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
